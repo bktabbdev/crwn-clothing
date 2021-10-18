3326fa2e3eeb67026cb7b37c24370abe03a8f531
Austin Tabb <geneatabb@Austins-MacBook-Pro.local>
2021-10-18 13:42:29 -0700

    updating our application with reselect, added memoization

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    console.log("this.props: ", this.props);
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,14 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { ReactComponent as ShoppingIcon } from "./../../assets/shopping-bag.svg";
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon">
     <ShoppingIcon className="shopping-icon" onClick={toggleCartHidden} />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,4 +20,15 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   itemCount: cartItems.reduce(
+//     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+//     0
+//   ),
+// });
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
