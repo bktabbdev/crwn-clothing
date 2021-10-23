549edd646a83f3e14d3a242c188deed1600cb89a
tabb-dev <tabbdevelopment@gmail.com>
2021-10-23 10:16:50 -0700

    added x

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route } from "react-router";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-const ShopPage = () => (
-  <div className="shop-page">
-    <CollectionsOverview />
-  </div>
-);
+import CollectionPage from "../category/collection.component";
+
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
