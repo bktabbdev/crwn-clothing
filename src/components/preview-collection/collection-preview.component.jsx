5705391f10559ff8d3839fd86aff350a1372017f
Austin Tabb <geneatabb@local-hostname.local>
2021-10-22 19:42:47 -0700

    new rewrite

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -6,7 +6,7 @@ import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1>{title}</h1>
+      <h1>{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((items, idx) => idx < 4)
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,12 +1,25 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // tells redux-persist i want to use loacl storage (alternative is sessionStorage)
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+
+const persistConfig = {
+  // JSON object specifiying options
+  key: "root", // root means at what point we want to start storing
+  storage,
+  whitelist: ["cart"], //string names of reducers we want to store - don't persist user because being handled by firebase
+};
 
 //user is key and goes to user we want
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
