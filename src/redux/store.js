dc0e532f1cbc350ecef52943454e6db7b8b60260
tabb-dev <tabbdevelopment@gmail.com>
2021-10-23 21:11:48 -0700

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
-
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
+
+if (process.env.NODE_ENV === "development") {
+  // CRA sets environment variable
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
